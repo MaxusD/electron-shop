53d6a252288ce236b81143103064960f27299635
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-03-17 11:53:38 +0200

    Fix server parameters

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const path = require('path')
 const userRouter = require('./routes/user.routes')
 const postRouter = require('./routes/post.routes')
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use(cors())
