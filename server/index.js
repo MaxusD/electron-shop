36f59507fdd4c06348ef72ab448e2beb127686d6
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-03-17 15:11:28 +0200

    Added credentials

--- server/index.js
+++ server/index.js
@@ -17,6 +17,7 @@ const app = express()
 app.use(cors({
     origin: "https://electron-shop-sigma.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
