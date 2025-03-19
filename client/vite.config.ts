1d46d578cc75447e002ce0b0ba88b2b0d0a682bf
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-03-19 17:10:08 +0200

    Change params for Vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   return {
     define: {
-      'process.env.REACT_APP_API_URL': JSON.stringify(env.REACT_APP_API_URL)
+      //'process.env.REACT_APP_API_URL': JSON.stringify(env.REACT_APP_API_URL)
+      'import.meta.REACT_APP_API_URL': JSON.stringify(env.REACT_APP_API_URL)
     },
     plugins: [react()],
     server: {
