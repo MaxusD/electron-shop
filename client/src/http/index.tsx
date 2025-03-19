f29f33203ae147af3d12a459641623212ac165f5
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-03-19 17:21:35 +0200

    Change params for Vercel

--- client/src/http/index.tsx
+++ client/src/http/index.tsx
@@ -1,12 +1,12 @@
 import axios from "axios"
 const $host = axios.create({
     //baseURL: process.env.REACT_APP_API_URL
-    baseURL: import.meta.env.REACT_APP_API_URL
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL
 })
 
 const $authHost = axios.create({
     //baseURL: process.env.REACT_APP_API_URL
-    baseURL: import.meta.env.REACT_APP_API_URL
+    baseURL: import.meta.env.VITE_REACT_APP_API_URL
 })
 
 const authInterceptor = config => {
@@ -15,7 +15,6 @@ const authInterceptor = config => {
 }
 
 $authHost.interceptors.request.use(authInterceptor)
-console.log("API_URL:", import.meta.env.REACT_APP_API_URL);
 
 
 export {
