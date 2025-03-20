227b89ddff110f7fcbbcaeb363ec19a33bd6a1d3
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-03-20 19:59:11 +0200

    add origin CORS

--- server/index.js
+++ server/index.js
@@ -16,16 +16,14 @@ const PORT = process.env.PORT || 5000
 const app = express()
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    next();
-});
-
-
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    next()
+})
 
 app.use(cors({
-    origin: /*"https://electron-shop-sigma.vercel.app"*/'*',
+    origin: "https://electron-shop-sigma.vercel.app",
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization']
