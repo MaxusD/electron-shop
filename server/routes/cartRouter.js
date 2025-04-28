b905db119401b680d40a5bed47732d19c0fc6ba5
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-04-28 11:55:51 +0300

    Checking for cart

--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -5,7 +5,9 @@ const cartController = require('../controller/cartController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.get('/', authMiddleware, cartController.getCart)
-router.post('/', authMiddleware, cartController.addToCart)
+router.post('/', (req, res, next) => {
+    console.log('POST /api/cart was hit')
+    next() }, authMiddleware, cartController.addToCart)
 router.delete('/device/:id', authMiddleware, cartController.removeFromCart)
 router.delete('/clear', authMiddleware, cartController.clearCart)
 router.post('/decrease', authMiddleware, cartController.decreaseQuantity)
