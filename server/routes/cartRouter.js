ddcd3731e7f8830e5254e2496d68c7ff70381b4e
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-04-29 16:22:09 +0300

    Fix encrease device

--- server/routes/cartRouter.js
+++ server/routes/cartRouter.js
@@ -5,9 +5,7 @@ const cartController = require('../controller/cartController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 router.get('/', authMiddleware, cartController.getCart)
-router.post('/', (req, res, next) => {
-    console.log('POST /api/cart was hit')
-    next() }, authMiddleware, cartController.addToCart)
+router.post('/', authMiddleware, cartController.addToCart)
 router.delete('/device/:id', authMiddleware, cartController.removeFromCart)
 router.delete('/clear', authMiddleware, cartController.clearCart)
 router.post('/decrease', authMiddleware, cartController.decreaseQuantity)
