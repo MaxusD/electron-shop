21cc274d5072f3506ac701f45d13213dc7238027
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-05-01 16:14:43 +0300

    Fix secure

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -102,7 +102,7 @@ class UserController {
 
             res.cookie('refreshToken', newRefreshToken, {
                 httpOnly: true,
-                secure: false, //true == https
+                secure: false,  //true == https
                 sameSite: 'none', //'strict' for localhost
                 maxAge: 30 * 24 * 60 * 60 * 1000
             })
