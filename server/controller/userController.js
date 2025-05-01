b3a3729b6a6033b594569b66e9c57a6e62c12bbf
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-05-01 16:06:41 +0300

    Fix loop again

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -103,7 +103,7 @@ class UserController {
             res.cookie('refreshToken', newRefreshToken, {
                 httpOnly: true,
                 secure: false, //true == https
-                sameSite: 'strict', //'strict' for localhost
+                sameSite: 'none', //'strict' for localhost
                 maxAge: 30 * 24 * 60 * 60 * 1000
             })
 
