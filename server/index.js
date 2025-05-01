62fe274b03e142fbb49dfbdac5d316880c1afd6e
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-05-01 12:58:44 +0300

    Fix loop 4

--- server/index.js
+++ server/index.js
@@ -14,14 +14,20 @@ const cors = require('cors')
 const PORT = process.env.PORT || 5000
 const app = express()
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "https://electron-shop-sigma.vercel.app")
-    //res.header("Access-Control-Allow-Origin", "http://localhost:5173")
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS")
-    res.header("Access-Control-Allow-Credentials", "true")
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
-    next()
-})
+// app.use((req, res, next) => {
+//     res.header("Access-Control-Allow-Origin", "https://electron-shop-sigma.vercel.app")
+//     //res.header("Access-Control-Allow-Origin", "http://localhost:5173")
+//     res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS")
+//     res.header("Access-Control-Allow-Credentials", "true")
+//     res.header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+//     next()
+// })
+
+app.use(cors({
+    origin: 'https://electron-shop-sigma.vercel.app',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}))
 
 app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.url}`)
