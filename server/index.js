d969cbb50815b520e33affad28d6ba478dea467f
unknown <C:\Users\Max\AppData\Roaming\The Bat!>
2025-05-01 12:25:14 +0300

    Fix loop

--- server/index.js
+++ server/index.js
@@ -23,10 +23,10 @@ app.use((req, res, next) => {
     next()
 })
 
-app.use((req, res, next) => {
+/*app.use((req, res, next) => {
     console.log(`Incoming request: ${req.method} ${req.url}`)
     next()
-})
+})*/
 
 app.use(cookieParser())
 app.use(express.json())
